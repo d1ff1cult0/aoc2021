3f9bef802e9f9b9a08b7aef62147f04890d30142
Jarne Plessers <jarne.plessers@student.kuleuven.be>
2021-12-01 10:18:48 +0100

    added solution

---
+++ Day1.js
@@ -0,0 +1,19 @@
+function amount_of_increased(){
+    let fs = require('fs');
+    let text = fs.readFileSync("/Users/d1ff1cult/Documents/KUL 2021-2022/aoc2021/input1.txt", 'utf-8');
+    let textByLine = text.split('\n')
+    let increased = 0
+    let decreased = 0
+
+    for(let i = 1; i<textByLine.length; i++){
+        if(textByLine[i] < textByLine[i+1]){
+            increased++
+        }else {
+            decreased++
+        }
+    }
+
+    return [increased, decreased, increased+decreased]
+}
+
+console.log(amount_of_increased())
