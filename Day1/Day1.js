df18d29e7dd244d9aa27a45b0cc3fc26f2b80247
Jarne Plessers <jarne.plessers@student.kuleuven.be>
2021-12-01 12:28:04 +0100

    Added part 2

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,19 +1,29 @@
-function amount_of_increased(){
-    let fs = require('fs');
-    let text = fs.readFileSync("/Users/d1ff1cult/Documents/KUL 2021-2022/aoc2021/Day1/input1.txt", 'utf-8');
-    let textByLine = text.split('\n')
-    let increased = 0
-    let decreased = 0
+let fs = require('fs');
+let text = fs.readFileSync("/Users/d1ff1cult/Documents/KUL 2021-2022/aoc2021/Day1/input1.txt", 'utf-8');
+let array = (text.split('\n')).map((i) => Number(i));
 
-    for(let i = 1; i<textByLine.length; i++){
-        if(textByLine[i] < textByLine[i+1]){
+function part1(){
+    let increased = 0
+    for(let i = 0; i<array.length; i++){
+        if(array[i] > array[i-1]){
             increased++
-        }else {
-            decreased++
         }
     }
-
-    return [increased, decreased, increased+decreased]
+    return increased
 }
 
-console.log(amount_of_increased())
+function part2(){
+    let increased = 0
+    for(let i = 0; i<array.length; i++){
+        if(i+3 < array.length){
+            let prev_sum = array[i]+array[i+1]+array[i+2]
+            let current_sum = array[i+1]+array[i+2]+array[i+3]
+            if(prev_sum < current_sum){
+                increased++
+            }
+        }
+    }
+    return increased
+}
+console.log(part1(), part2())
+
