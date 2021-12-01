cc7cecefb7892e14f1ee6635bbc51bb0b966ca92
Jarne Plessers <jarne.plessers@student.kuleuven.be>
2021-12-01 10:20:18 +0100

    added folder

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,6 +1,6 @@
 function amount_of_increased(){
     let fs = require('fs');
-    let text = fs.readFileSync("/Users/d1ff1cult/Documents/KUL 2021-2022/aoc2021/input1.txt", 'utf-8');
+    let text = fs.readFileSync("/Users/d1ff1cult/Documents/KUL 2021-2022/aoc2021/Day1/input1.txt", 'utf-8');
     let textByLine = text.split('\n')
     let increased = 0
     let decreased = 0
